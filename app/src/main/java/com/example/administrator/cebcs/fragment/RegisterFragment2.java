75cf9302cb016c0497a93954e79a478a9e4cd3b2
MasterUNG <phrombutr@gmail.com>
2017-11-29 10:57:49 +0700

    Back OK

--- app/src/main/java/com/example/administrator/cebcs/fragment/RegisterFragment2.java
+++ app/src/main/java/com/example/administrator/cebcs/fragment/RegisterFragment2.java
@@ -3,10 +3,12 @@ package com.example.administrator.cebcs.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.administrator.cebcs.MainActivity;
 import com.example.administrator.cebcs.R;
 
 /**
@@ -14,6 +16,37 @@ import com.example.administrator.cebcs.R;
  */
 
 public class RegisterFragment2 extends Fragment{
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Toolbar
+        toolbar();
+
+//        Save Controller
+
+
+    }   // Main Method
+
+    private void toolbar() {
+        Toolbar toolbar = getView().findViewById(R.id.toolberRegister2);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.register));
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle(getString(R.string.message_have_space));
+
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+
+    }
 
     @Nullable
     @Override
